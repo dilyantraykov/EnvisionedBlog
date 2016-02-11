7b8078f3071fbdffa7c0cfe0fe23842fc8e7a9e1
dilyantraykov <dtraykov94@gmail.com>
2016-02-11 23:25:44 +0200

    Added new database migration

---
+++ Source/EnvisionedBlog.Data/Migrations/201602112101223_AvatarsAdded.cs
@@ -0,0 +1,16 @@
+namespace EnvisionedBlog.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AvatarsAdded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
